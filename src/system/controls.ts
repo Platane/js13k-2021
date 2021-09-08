d89a30c7f3caa724ab4485a777d5f197986d574f
platane <me@platane.me>
2021-09-08 23:46:05 +0200

    🚀 add select blob on tap control

--- src/system/controls.ts
+++ src/system/controls.ts
@@ -1,8 +1,11 @@
+import { vec2 } from "gl-matrix";
+import { isInsideBlob } from "../math/gauss";
+import { getPack } from "../math/pack";
 import { Vec2 } from "../math/types";
 import { state } from "../state";
-import { unProjX, unProjY } from "./camera";
+import { unProj, unProjX, unProjY } from "./camera";
 
-const getCollisionFunction = ([[ax, ay], [bx, by]]: [Vec2, Vec2]) => {
+const getBoxCollisionFunction = ([[ax, ay], [bx, by]]: [Vec2, Vec2]) => {
   const xMin = Math.min(ax, bx);
   const xMax = Math.max(ax, bx);
   const yMin = Math.min(ay, by);
@@ -14,21 +17,47 @@ const getCollisionFunction = ([[ax, ay], [bx, by]]: [Vec2, Vec2]) => {
 const selectAction = Symbol();
 const moveAction = Symbol();
 let action = null as null | typeof selectAction | typeof moveAction;
+let downTimestamp = 0;
 
-const onMouseDown = ({ pageX, pageY, button, ctrlKey }: MouseEvent) => {
+const blobHit = (p: Vec2, positions: Vec2[]) => {
+  if (!isInsideBlob(positions, p[0], p[1])) return null;
+
+  let minD = Infinity;
+  let minI = 0;
+
+  positions.forEach((o, i) => {
+    const d = vec2.distance(o, p);
+
+    if (d < minD) {
+      minD = d;
+      minI = i;
+    }
+  });
+
+  return getPack(positions, minI);
+};
+
+const onMouseDown = ({
+  pageX,
+  pageY,
+  button,
+  ctrlKey,
+  timeStamp,
+}: MouseEvent) => {
+  action = null;
+  downTimestamp = timeStamp;
+
   switch (button) {
     case 0: {
-      action = selectAction;
+      const p = unProj([0, 0], pageX, pageY);
 
-      state.selection.rect = [
-        [unProjX(pageX), unProjY(pageY)],
-        [unProjX(pageX), unProjY(pageY)],
-      ];
+      action = selectAction;
+      state.selection.rect = [p, p.slice() as Vec2];
       state.selection.particlesIndexes = null;
+
+      break;
     }
     case 2: {
-      action = selectAction;
-
       const indexes = state.selection.particlesIndexes;
 
       if (!indexes) return;
@@ -46,6 +75,8 @@ const onMouseDown = ({ pageX, pageY, button, ctrlKey }: MouseEvent) => {
         indexes,
         targets: [[unProjX(pageX), unProjY(pageY)]],
       });
+
+      break;
     }
   }
 };
@@ -56,7 +87,7 @@ const onMouseMove = ({ pageX, pageY }: MouseEvent) => {
   state.selection.rect[1][0] = unProjX(pageX);
   state.selection.rect[1][1] = unProjY(pageY);
 
-  const isHit = getCollisionFunction(state.selection.rect);
+  const isHit = getBoxCollisionFunction(state.selection.rect);
 
   state.selection.particlesIndexes = state.particlesPositions[0].reduce(
     (arr, p, i) => (isHit(p) ? [...arr, i] : arr),
@@ -64,8 +95,23 @@ const onMouseMove = ({ pageX, pageY }: MouseEvent) => {
   );
 };
 
-const onMouseUp = ({ pageX, pageY }: MouseEvent) => {
+const onMouseUp = ({ pageX, pageY, timeStamp }: MouseEvent) => {
+  if (
+    action === selectAction &&
+    timeStamp - downTimestamp < 240 &&
+    (!state.selection.rect ||
+      vec2.distance(state.selection.rect[0], state.selection.rect[1]) <
+        20 / state.camera.a)
+  ) {
+    const p = unProj([0, 0], pageX, pageY);
+
+    const pack = blobHit(p, state.particlesPositions[0]);
+
+    state.selection.particlesIndexes = pack;
+  }
+
   state.selection.rect = null;
+  action = null;
 };
 document.addEventListener("mousemove", onMouseMove);
 document.addEventListener("mousedown", onMouseDown);
